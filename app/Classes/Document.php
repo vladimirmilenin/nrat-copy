9a62562ae97016450217385f6bf87ff7b0f5f4eb
mel <vladimir.milenin@gmail.com>
2023-09-29 13:37:09 +0300

    Commit

--- app/Classes/Document.php
+++ app/Classes/Document.php
@@ -2,8 +2,9 @@
 namespace App\Classes;
 
 use App\Helpers\Helpers;
-use Doctrine\Inflector\Rules\Pattern;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class Document {
 
@@ -15,26 +16,55 @@ class Document {
 
     public $registrationNumber;
     public $documentType;
-    public $attributes;
+    public $document;
 
     private $documentUrl;
+    private $yearFolder;
 
     public function __construct(string $registrationNumber){
         $this->registrationNumber = $registrationNumber;
         $this->documentUrl = Str::finish(config('nrat.documents_endpoint'), '/') . $this->registrationNumber;
-        $this->getRemote();
-        dd($this->documentUrl);
-        dd($this->registrationNumber);
+        $this->yearFolder = $this->getYearFolder();
+
+        $this->document = $this->getDocument();
+        dd($this->document);
+    }
+
+
+    private function getDocument(){
+        $document = $this->getFromCache();
+        if ($this->validationDoument($document)){
+            return $document;
+        } else {
+            return $this->getRemote();
+        }
+    }
+
+    private function setToCache(string $src){
+        Storage::put('/cache/documents/' . $this->yearFolder . '/' . $this->registrationNumber, $src);
+    }
+
+    private function getFromCache(){
+        return Storage::json('/cache/documents/' . $this->yearFolder . '/' . $this->registrationNumber);
     }
 
     private function getRemote(){
-        $doc = Helpers::getUrl($this->documentUrl);
-        dd(json_decode($doc));
-
-        // if (empty($doc = Helpers::getUrl($this->documentUrl))){
-        //     abort(404);
-        // }
-        // dd($foo);
+        $src = Helpers::getUrl($this->documentUrl);
+        $document = json_decode($src, true);
+        if ($this->validationDoument($document)){
+            $this->setToCache($src);
+            return $document;
+        }
+        return null;
     }
+
+    private function validationDoument(mixed $document){
+        return !empty($document['registration_number']);
+    }
+
+    private function getYearFolder(){
+        return Str::substr($this->registrationNumber, 2, 2);
+    }
+
 
 }

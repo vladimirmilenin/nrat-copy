6fb491a587e424e4417e48450d5bac33323ab4a2
mel <vladimir.milenin@gmail.com>
2023-11-06 15:20:48 +0200

    Commit

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="px-4 py-5 my-5 text-center">
-    <div class="display-1 text-brand">Search {{ app()->getLocale() }}</div>
+    <div class="display-1 text-brand">Вже скоро буде</div>
 </div>
 
 @endsection
